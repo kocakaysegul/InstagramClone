23edeabc779baf5551d28850e8a02262e5886b73
kocakaysegul <aysegulkocak112@gmail.com>
2023-03-02 08:38:58 +0300

    user interface updated

--- instagram/ViewController.swift
+++ instagram/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
 
-
+    @IBAction func signInClicked(_ sender: Any) {
+        performSegue(withIdentifier: "toFeedVC", sender: nil)
+    }
+    
+    
+    @IBAction func signUpClicked(_ sender: Any) {
+    }
 }
 
