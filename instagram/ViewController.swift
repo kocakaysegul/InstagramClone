f507d36a58285d432a720e3a9c902e74cd5ea35a
kocakaysegul <aysegulkocak112@gmail.com>
2023-03-15 09:20:18 +0300

    Sign up completed

--- instagram/ViewController.swift
+++ instagram/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
@@ -21,12 +22,32 @@ class ViewController: UIViewController {
     
     
 
-    @IBAction func signInClicked(_ sender: Any) {
+    @IBAction func signInClicked(_ sender: Any) { // log in
         performSegue(withIdentifier: "toFeedVC", sender: nil)
     }
     
     
-    @IBAction func signUpClicked(_ sender: Any) {
+    @IBAction func signUpClicked(_ sender: Any) { //register
+        
+        if emailText.text != "" && passwordText.text != "" {
+            Auth.auth().createUser(withEmail: emailText.text!, password: passwordText.text!) { authData, error in
+                if error != nil {
+                    self.makeAlert(titleInput: "Error", messageInput: error!.localizedDescription ?? "Error!")
+                } else {
+                    self.performSegue(withIdentifier: "toFeedVC", sender: nil)
+                }
+            }
+        } else {
+            makeAlert(titleInput: "Error", messageInput: "Username/Password?")
+        }
+        
+    }
+    
+    func makeAlert (titleInput: String, messageInput : String) {
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default)
+        alert.addAction(okButton)
+        self.present(alert, animated: true)
     }
 }
 
