96e6e13f593445ea4449053f13d5637aff1f0214
kocakaysegul <aysegulkocak112@gmail.com>
2023-03-18 15:40:10 +0300

    storage updated

--- instagram/UploadViewController.swift
+++ instagram/UploadViewController.swift
@@ -37,6 +37,13 @@ class UploadViewController: UIViewController, UIImagePickerControllerDelegate, U
         imageView.image = info[.originalImage] as? UIImage
         self.dismiss(animated: true, completion: nil)
     }
+    
+    func makeAlert(titleInput : String, messageInput : String) {
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil)
+    }
 
 
     @IBAction func actionButtonClicked(_ sender: Any) {
@@ -48,16 +55,22 @@ class UploadViewController: UIViewController, UIImagePickerControllerDelegate, U
         
         if let data = imageView.image?.jpegData(compressionQuality: 0.5) {
             
-            let imageReference = mediaFolder.child("image.jpg")
+            let uuid = UUID().uuidString
+            
+            let imageReference = mediaFolder.child("\(uuid).jpeg")
             
             imageReference.putData(data, metadata: nil) { metadata, error in
                 if error != nil {
-                    print(error?.localizedDescription)
+                    self.makeAlert(titleInput: "Error!", messageInput: error?.localizedDescription ?? "error")
                 } else {
                     imageReference.downloadURL { url, error in
                         if error == nil {
                             let imageUrl = url?.absoluteString
-                            print(imageUrl)
+                            
+                            
+                            //DATABASE
+                            
+                            
                         }
                     }
                 }
