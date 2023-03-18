6e61cde42fc1fb678f56409d6f5510b0a94459be
kocakaysegul <aysegulkocak112@gmail.com>
2023-03-18 19:27:15 +0300

    posts ordered

--- instagram/FeedViewController.swift
+++ instagram/FeedViewController.swift
@@ -34,7 +34,7 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         
         let fireStoreDatabase = Firestore.firestore()
         
-        fireStoreDatabase.collection("Posts").addSnapshotListener { snapshot, error in
+        fireStoreDatabase.collection("Posts").order(by: "date", descending: true).addSnapshotListener { snapshot, error in
             
             if error != nil {
                 print(error?.localizedDescription)
